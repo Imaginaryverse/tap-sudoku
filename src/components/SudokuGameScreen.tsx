67f53319f4506f20163874368bc395000b9720e8
Anton Bertilsson <bertilsson.anton@gmail.com>
2021-10-17 01:22:13 +0200

    Style changes

--- src/components/SudokuGameScreen.tsx
+++ src/components/SudokuGameScreen.tsx
@@ -30,6 +30,25 @@ const SudokuBoard: FC = () => {
     setLocalBoard(copy);
   }
 
+  function isGray(row: number, col: number) {
+    if (row === 3 || row === 4 || row === 5) {
+      if (col === 3 || col === 4 || col === 5) {
+        return 'is-gray';
+      }
+    } else if (
+      col === 0 ||
+      col === 1 ||
+      col === 2 ||
+      col === 6 ||
+      col === 7 ||
+      col === 8
+    ) {
+      return 'is-gray';
+    } else {
+      return '';
+    }
+  }
+
   useEffect(() => {
     let interval: any;
 
@@ -58,7 +77,9 @@ const SudokuBoard: FC = () => {
                   <tr className='board-row' key={r}>
                     {row.map((cell, c) => (
                       <td
-                        className={`board-cell ${cell.locked ? 'locked' : ''}`}
+                        className={`board-cell ${isGray(r, c)} ${
+                          cell.locked ? 'locked' : ''
+                        } ${isCorrectSolution ? 'done' : ''}`}
                         onClick={
                           !cell.locked ? () => increment(r, c) : undefined
                         }
@@ -66,11 +87,11 @@ const SudokuBoard: FC = () => {
                       >
                         <p
                           className={`cell-value ${
-                            cell.incorrect ? 'incorrect' : null
+                            cell.incorrect ? 'incorrect' : ''
                           } ${
                             isCorrectSolution && !cell.locked && !cell.incorrect
                               ? 'correct'
-                              : null
+                              : ''
                           }`}
                         >
                           {cell.value ? cell.value : null}
